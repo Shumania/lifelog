119a15452f55e6dd3179589f94f3b2e5e6e4687b
Andrew Shuman <andrew_shuman@hotmail.com>
2026-05-26 14:44:56 -0700

    dev_next: grab SSE debug lines from log

--- dev_next.ps1
+++ dev_next.ps1
@@ -1,15 +1,8 @@
-# Check SSE POST results in log
-$log = 'C:\ProgramData\LifeLog\lifelog_service.log'
+# Grab SSE debug lines from service log
+$log = "$env:ProgramData\LifeLog\lifelog_service.log"
 if (Test-Path $log) {
-    $lines = Get-Content $log -Tail 200
-    $sse = $lines | Select-String -Pattern 'SSE'
-    if ($sse) {
-        Write-Output "=== All SSE log lines (last 200) ==="
-        $sse | ForEach-Object { $_.Line }
-    } else {
-        Write-Output "No SSE lines in last 200 log lines"
-        $lines | Select-Object -Last 15
-    }
+    $lines = Get-Content $log -Tail 100 | Where-Object { $_ -match 'SSE|ntfy_ui|publish_ui' }
+    if ($lines) { $lines -join "`n" } else { 'No SSE lines found in last 100 log lines' }
 } else {
-    Write-Output "Log file not found"
+    'Log file not found'
 }
